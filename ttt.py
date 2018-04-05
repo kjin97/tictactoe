6c71761ecae6baf034940662d5d22a80dfc37fd7
Kevin Jin <kjin@kevins-mbp-2.devices.brown.edu>
2018-04-04 22:55:16 -0400

    gui complete

--- ttt.py
+++ ttt.py
@@ -1,9 +1,9 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Button, PhotoImage, Label #, Text
 
 class ticTacToe:
 	def __init__(self, multiplayer = False):
 		self.over = False
-		self._board = [[0,0,0], [0,0,0], [0,0,0]]
+		self.board = [[0,0,0], [0,0,0], [0,0,0]]
 		self.winner = 0
 		self._turn = 0 # increments every square played
 		self.to_play = 1 # 1 for player 1, -1 for player 2
@@ -11,42 +11,42 @@ class ticTacToe:
 
 	def play(self, row, col):
 		# returns True if unplayable on square, else no return value.
-		if self._board[row][col]:
+		if self.board[row][col]:
 			return True
 		else:
 			self._play_on(row, col)
 
 	def _play_on(self, row, col):
-		self._board[row][col] = self.to_play
+		self.board[row][col] = self.to_play
 		self._turn += 1
 		self.to_play = -self.to_play
 		self._check()
 
 	def _undo_move(self, row, col):
-		self._board[row][col] = 0
+		self.board[row][col] = 0
 		self._turn -= 1
 		self.to_play = -self.to_play
 
 	def _check(self):
-		for row in self._board:
+		for row in self.board:
 			if sum(row) == 3:
 				return self._win(1)
 			if sum(row) == -3:
 				return self._win(-1)
 		for i in range(3):
-			if sum([row[i] for row in self._board]) == 3:
+			if sum([row[i] for row in self.board]) == 3:
 				return self._win(1)
-			if sum([row[i] for row in self._board]) == -3:
+			if sum([row[i] for row in self.board]) == -3:
 				return self._win(-1)
-		if self._board[1][1] == 1:
-			if self._board[0][2] + self._board[2][0] == 2:
+		if self.board[1][1] == 1:
+			if self.board[0][2] + self.board[2][0] == 2:
 				return self._win(1)
-			if self._board[0][0] + self._board[2][2] == 2:
+			if self.board[0][0] + self.board[2][2] == 2:
 				return self._win(1)
-		if self._board[1][1] == -1:
-			if self._board[0][2] + self._board[2][0] == -2:
+		if self.board[1][1] == -1:
+			if self.board[0][2] + self.board[2][0] == -2:
 				return self._win(-1)
-			if self._board[0][0] + self._board[2][2] == -2:
+			if self.board[0][0] + self.board[2][2] == -2:
 				return self._win(-1)
 		if self._turn == 9:
 			return self._win(0)
@@ -57,7 +57,7 @@ class ticTacToe:
 		return player
 
 	def print_board(self):
-		for row in self._board:
+		for row in self.board:
 			line = ' '
 			for square in row:
 				if square > 0:
@@ -65,7 +65,7 @@ class ticTacToe:
 				elif square < 0:
 					line += ('O ')
 				else:
-					line += ('  ')
+					line += ('_ ')
 			print(line)
 
 	# Returns a tuple of optimal move and minimax value
@@ -84,7 +84,7 @@ class ticTacToe:
 			best_move = None
 			for i in range(3):
 				for j in range(3):
-					if not self._board[i][j]:
+					if not self.board[i][j]:
 						self._play_on(i, j)
 						move_val = self.minimax()[1]
 						if move_val >= best_val:
@@ -97,7 +97,7 @@ class ticTacToe:
 			best_move = None
 			for i in range(3):
 				for j in range(3):
-					if not self._board[i][j]:
+					if not self.board[i][j]:
 						self._play_on(i, j)
 						move_val = self.minimax()[1]
 						if move_val <= best_val:
@@ -111,64 +111,64 @@ class ticTacToe:
 		i, j = self.minimax()[0]
 		self._play_on(i, j)
 		# for i in range(3):
-		# 	if sum(self._board[i]) == 2 * player:
-		# 		j = self._board[i].index(0)
+		# 	if sum(self.board[i]) == 2 * player:
+		# 		j = self.board[i].index(0)
 		# 		return self._play_on(i, j)
 		# for j in range(3):
-		# 	if sum([row[j] for row in self._board]) == 2 * player:
-		# 		i = [row[j] for row in self._board].index(0)
+		# 	if sum([row[j] for row in self.board]) == 2 * player:
+		# 		i = [row[j] for row in self.board].index(0)
 		# 		return self._play_on(i, j)
 
 		# for i in range(3):
-		# 	if sum(self._board[i]) == 2 * -player:
-		# 		j = self._board[i].index(0)
+		# 	if sum(self.board[i]) == 2 * -player:
+		# 		j = self.board[i].index(0)
 		# 		return self._play_on(i, j)
 		# for j in range(3):
-		# 	if sum([row[j] for row in self._board]) == 2 * -player:
-		# 		i = [row[j] for row in self._board].index(0)
+		# 	if sum([row[j] for row in self.board]) == 2 * -player:
+		# 		i = [row[j] for row in self.board].index(0)
 		# 		return self._play_on(i, j)
 
-		# if self._board[1][1]:
+		# if self.board[1][1]:
 		# 	for i in range(3):
 		# 		for j in range(3):
-		# 			if not self._board[i][j]:
-		# 				if (sum([row[j] for row in self._board]) == player) and \
-		# 				sum(self._board[i]) == player:
+		# 			if not self.board[i][j]:
+		# 				if (sum([row[j] for row in self.board]) == player) and \
+		# 				sum(self.board[i]) == player:
 		# 					return self._play_on(i, j)
-		# 				if (sum([row[j] for row in self._board]) == -player) and \
-		# 				sum(self._board[i]) == -player:
+		# 				if (sum([row[j] for row in self.board]) == -player) and \
+		# 				sum(self.board[i]) == -player:
 		# 					return self._play_on(i, j)
 
-		# if self._board[1][1]:
+		# if self.board[1][1]:
 
 		# # 	for i in range(3):
-		# # 		if sum(self._board[i]) == player:
+		# # 		if sum(self.board[i]) == player:
 		# # 			for j in range(3):
-		# # 				if not self._board[i][j]:
+		# # 				if not self.board[i][j]:
 		# # 					return self._play_on(i, j)
 		# # 	for j in range(3):
-		# # 		if sum([row[j] for row in self._board]) == player:
+		# # 		if sum([row[j] for row in self.board]) == player:
 		# # 			for i in range(3):
-		# # 				if not self._board[i][j]:
+		# # 				if not self.board[i][j]:
 		# # 					return self._play_on(i, j)
 		# # 	for i in range(3):
-		# # 		if sum(self._board[i]) == -player:
+		# # 		if sum(self.board[i]) == -player:
 		# # 			for j in range(3):
-		# # 				if not self._board[i][j]:
+		# # 				if not self.board[i][j]:
 		# # 					return self._play_on(i, j)
 		# # 	for j in range(3):
-		# # 		if sum([row[j] for row in self._board]) == -player:
+		# # 		if sum([row[j] for row in self.board]) == -player:
 		# # 			for i in range(3):
-		# # 				if not self._board[i][j]:
+		# # 				if not self.board[i][j]:
 		# # 					return self._play_on(i, j)
 				
-		# 	if self._board[0][0]:
-		# 		if self._board[2][0]:
-		# 			if self._board[0][2]:
-		# 				if self._board[2][2]:
-		# 					if self._board[1][0]:
-		# 						if self._board[0][1]:
-		# 							if self._board[1][2]:
+		# 	if self.board[0][0]:
+		# 		if self.board[2][0]:
+		# 			if self.board[0][2]:
+		# 				if self.board[2][2]:
+		# 					if self.board[1][0]:
+		# 						if self.board[0][1]:
+		# 							if self.board[1][2]:
 		# 								return self._play_on(2, 1)
 		# 							else:
 		# 								return self._play_on(1, 2)
@@ -191,24 +191,112 @@ class GUI:
 	def __init__(self):
 		self._window = Tk()
 		self._window.title('Tic Tac Toe')
-		self._board = ticTacToe()
+		self._game = ticTacToe()
+		self._AI = False
 		self._grid = [[None,None,None] for i in range(3)]
+		self._X = PhotoImage(file = 'x.gif')
+		self._O = PhotoImage(file = 'o.gif')
+		self._blank = PhotoImage(file = 'blank.gif')
+		self._win_dialog = None
 		for i in range(3):
 			for j in range(3):
 				self._grid[i][j] = self._createButton(i, j)
-		F2 = lambda: self._newGame
-		new_button = Button(self._window, command = F2)
+		sp_button = Button(self._window, command = self._single_player, width = 16, text = 'New 1 Player Game')
+		sp_button.grid(row = 3, column = 1)
+		mp_button = Button(self._window, command = self._multi_player, width = 16, text = 'New 2 Player Game')
+		mp_button.grid(row = 4, column = 1)
+		# self.win_msg = Text(self._window, font = ("Verdana", 32), width = 12, height = 1)
+		# self.win_msg.tag_configure("center", justify='center')
+
+		self._window.mainloop()
 
 
 	def _createButton(self, row, col):
-		play = lambda: self._board.play(row, col) 
-		new_button = Button(self._window, command = play)
-		new_button.grid(row = x, col = y)
+		
+		def play():
+			self._game.play(row, col)
+			self._update()
+			if self._AI and not self._game.over:
+				self._game.AI_play()
+				self._update()
+
+		new_button = Button(self._window, command = play, width = 160, height = 160, image = self._blank)
+		new_button.grid(row = row, column = col)
+		new_button.image = self._blank
+		return new_button
 
 	def _update(self):
-		pass
-		
+		for row in range(3):
+			for col in range(3):
+				if self._game.board[row][col]:
+					self._grid[row][col].config(state = 'disabled')
+					if self._game.board[row][col] > 0:
+						self._grid[row][col].config(image = self._X)
+						self._grid[row][col].image = self._X
+					if self._game.board[row][col] < 0:
+						self._grid[row][col].config(image = self._O)
+						self._grid[row][col].image = self._O
+				else:
+					self._grid[row][col].config(state = 'normal')
+					self._grid[row][col].config(image = self._blank)
 
+
+		self._game.print_board()
+		if self._game.over:
+			for i in range(3):
+				for j in range(3):
+					self._grid[i][j].config(state = 'disabled')
+			if self._AI:
+				if self._game.winner > 0:
+					# self.win_msg.insert(1.0, 'You Win!') # JK it'll never happen
+					win_msg = 'You Win!'
+				elif self._game.winner < 0:
+					# self.win_msg.insert(1.0, 'You Lose!')
+					win_msg = 'You Lose!'
+				else:
+					# self.win_msg.insert(1.0, 'It\'s a Draw!')
+					win_msg = 'It\'s a Draw!'
+			else:
+				if self._game.winner > 0:
+					# self.win_msg.insert(1.0, 'Player 1 Wins!')
+					win_msg = 'Player 1 Wins!'
+				elif self._game.winner < 0:
+					# self.win_msg.insert(1.0, 'Player 2 Wins!')
+					win_msg = 'Player 2 Wins!'
+				else:
+					#self.win_msg.insert(1.0, 'It\'s a Draw!')
+					win_msg = 'It\'s a Draw!'
+			# self.win_msg.tag_add("center", "1.0", "end")
+			# self.win_msg.place(relx = 0.5, rely = 0.5)
+
+			self._win_dialog = Label(self._window, text = win_msg, font = ('Helvetica', 32), justify = 'center')
+			self._win_dialog.place(relx = 0.5, rely = 0.5, anchor = 'c')
+
+
+	def _single_player(self):
+		self._AI = True
+		self._game = ticTacToe()
+		if self._win_dialog:
+			self._win_dialog.destroy()
+			self._win_dialog = None
+		for i in range(3):
+			for j in range(3):
+				self._grid[i][j].config(state = 'normal')
+				self._grid[i][j].config(state = 'normal')
+		self._update()
+
+
+	def _multi_player(self):
+		self._AI = False
+		self._game = ticTacToe()
+		if self._win_dialog:
+			self._win_dialog.destroy()
+			self._win_dialog = None
+		for i in range(3):
+			for j in range(3):
+				self._grid[i][j].config(state = 'normal')
+				self._grid[i][j].config(state = 'normal')
+		self._update()
 
 
 def read_input():
@@ -218,12 +306,13 @@ def read_input():
 		exit()
 
 def main():
+	gui = GUI()
+	players = 0
+	while players not in ['1', '2']:
+		print('How many players?')
+		players = read_input()
+	players = int(players)
 	while True:
-		players = 0
-		while players not in ['1', '2']:
-			print('How many players?')
-			players = read_input()
-		players = int(players)
 		if players == 1:
 			winner = single_player()
 		else:
@@ -243,6 +332,14 @@ def single_player():
 	rows = ['3', '2', '1']
 	game = ticTacToe()
 
+	answers = ['y', 'n']
+	response = ''
+	while response not in answers:
+		print('Would you like to start? Type y for yes and n for no')
+		response = read_input()[0]
+	if response == 'n':
+		game.play(1, 1)
+		game.print_board()
 	while not game.over:
 		print('Where would you like to play?')
 		square = read_input()
